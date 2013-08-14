8ba7931293bbef9c76e635277a156ef0b404d5eb
Makis Tracend <makis.tracend@gmail.com>
2013-08-14 16:07:45 -0700

    running initialize of parent class

--- assets/js/backbone.ui.litenav.js
+++ assets/js/backbone.ui.litenav.js
@@ -3,55 +3,57 @@
 // Created by: Lyndel Thomas (@ryndel)
 // Source: https://github.com/backbone-ui/litenav
 //
-// Licensed under the MIT license: 
+// Licensed under the MIT license:
 // http://makesites.org/licenses/MIT
 
 (function(_, Backbone) {
-	
+
 	// fallbacks
 	if( _.isUndefined( Backbone.UI ) ) Backbone.UI = {};
 	// Support backbone app (if available)
 	var View = ( typeof APP != "undefined" && !_.isUndefined( APP.View) ) ? APP.View : Backbone.View;
-	
+
 	Backbone.UI.Litenav = View.extend({
-		
+
 		el : '.ui-litenav',
-		
+
 		options : {
-			navEl : ".nav", 
+			navEl : ".nav"
 		},
-		
-		initialize: function(){
+
+		initialize: function( options ){
 			var self = this;
 			window.addEventListener('resize', function(){ self.resize() }, false);
+
+			return View.prototype.initialize.call(this, options);
 		},
-		
+
 		events: {
 			"click .ui-litenav-control": "toggle",
 		},
-		
+
 		toggle: function(e) {
 			e.preventDefault();
 			$( this.options.navEl ).toggleClass('ui-litenav-active');
 		},
-		
+
 		resize: function() {
 			$( this.options.navEl ).removeClass('ui-litenav-active');
 		},
-		
+
 		postRender: function() {
 			// check if we have the sidenav control
 			var control = $(this.el).find(".ui-litenav-control");
-			if( !control.length ) { 
+			if( !control.length ) {
 				// add it to the el
 				var $el = $('<a href="" class="ui-litenav-control"><img src="../assets/img/menu-dark.svg"></a>');
 				$(this.el).find("header").prepend($el);
 				$(this.el).prepend($el);
-				
+
 				$( this.options.navEl ).addClass("ui-litenav-target");
-				
+
 			}
-		} 
+		}
 	});
-	
+
 })(this._, this.Backbone);
